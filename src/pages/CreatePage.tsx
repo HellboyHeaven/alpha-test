06f68188bcd37bce1d30cf4f281a64c653c2d01d
Yersultan Kylyshev <ersultan272001@gmail.com>
2025-11-25 16:16:44 +0100

    fix home back button

--- src/pages/CreatePage.tsx
+++ src/pages/CreatePage.tsx
@@ -1,11 +1,13 @@
 import { useNavigate } from "react-router-dom";
 import CreateProductForm from "../components/CreateProductForm";
+import HomeBackButton from "../components/HomeBackButton";
 
 export default function CreatePage() {
-    const nav = useNavigate()
-    return (
-        <div className="w-screen h-screen bg-white flex items-center">
-            <CreateProductForm onBack={() => nav('/')}/>
-        </div>
-    )
+  const nav = useNavigate();
+  return (
+    <div className="w-screen h-screen p-10 bg-white grid flex items-center flex-col gap-1">
+      <CreateProductForm onBack={() => nav("/")} />
+      <HomeBackButton />
+    </div>
+  );
 }
--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -5,32 +5,36 @@ import ProductDetailsView from "../components/ProductDetailsView";
 import HomeBackButton from "../components/HomeBackButton";
 
 export default function ProductPage() {
-    const { id } = useParams();
-    const nav = useNavigate();
-    const {products, addProduct} = useProductStore();
-    const product = products.find((p) => p.id === Number(id));
+  const { id } = useParams();
+  const nav = useNavigate();
+  const { products, addProduct } = useProductStore();
+  const product = products.find((p) => p.id === Number(id));
 
-    useEffect(() => {
-        fetch(`https://fakestoreapi.com/products/${id}`)
-          .then(res => res.json())
-          .then(data => addProduct(data))
-          .catch(err => console.error('Failed to fetch products:', err));
-      }, []);
+  useEffect(() => {
+    fetch(`https://fakestoreapi.com/products/${id}`)
+      .then((res) => res.json())
+      .then((data) => addProduct(data))
+      .catch((err) => console.error("Failed to fetch products:", err));
+  }, []);
 
-    if (!product) {
+  if (!product) {
     return (
       <div className="text-center py-12">
         <p className="text-gray-600 mb-4">Product not found</p>
-        <button onClick={ () => nav('/')} className="text-blue-600 hover:underline">
+        <button
+          onClick={() => nav("/")}
+          className="text-blue-600 hover:underline"
+        >
           Go back
         </button>
       </div>
     );
   }
 
-    return <div className="min-h-screen bg-white items-center p-10 flex flex-col space-y-10">
-         <HomeBackButton/>
-        <ProductDetailsView id={Number(id)} />
-       
+  return (
+    <div className="min-h-screen bg-white items-center p-10 flex flex-col space-y-10">
+      <ProductDetailsView id={Number(id)} />
+      <HomeBackButton />
     </div>
+  );
 }

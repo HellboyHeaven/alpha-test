1338731ebf1a70f4c257594fb7e259c892e16064
Yersultan Kylyshev <ersultan272001@gmail.com>
2025-11-25 16:43:59 +0100

    fix 404 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from "./App.tsx";
+import { HashRouter as Router } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HashRouter basename={import.meta.env.BASE_URL}>
+    <Router basename={import.meta.env.BASE_URL}>
       <App />
-    </HashRouter>
+    </Router>
   </StrictMode>
 );
